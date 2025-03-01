bd236f32aa59b85d8885280cfa9e178efd6c89c9
DevGamer9991 <pathornton114@gmail.com>
2025-03-01 02:36:45 -0600

    Update

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function getAuthenticatedClient() {
                 reject(e);
             }
             })
-            .listen(3000, () => {
+            .listen(5369, () => {
                 // open the browser to the authorize url to start the workflow
                 open(authorizeUrl, {wait: false}).then(cp => cp.unref());
             });
